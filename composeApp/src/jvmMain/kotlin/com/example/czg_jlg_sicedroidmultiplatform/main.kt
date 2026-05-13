6398bdfc6c7ddaf9111e52a09ab5cc3607af360c
elcalebitsur <s22120212@alumnos.itsur.edu.mx>
2026-05-13 14:34:39 -0600

    Configuración de la ventana (Desktop)

--- composeApp/src/jvmMain/kotlin/com/example/czg_jlg_sicedroidmultiplatform/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/czg_jlg_sicedroidmultiplatform/main.kt
@@ -2,11 +2,14 @@ package com.example.czg_jlg_sicedroidmultiplatform
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.unit.dp
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "CZG_JLG_SICEDroidMultiplatform",
+        title = "SICEDroid - ITSUR",
+        state = rememberWindowState(width = 400.dp, height = 800.dp)
     ) {
         App()
     }
